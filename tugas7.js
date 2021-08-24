555827ee54408d3187d05096a22ce85804595e99
lahiardhan <lahia.ardhanm@gmail.com>
2021-08-25 06:36:06 +0700

    Tugas 7 JS
    Tugas ini saya serahkan kepada Tim Niomic

---
+++ tugas7.js
@@ -0,0 +1,11 @@
+var tinggiBadan = [175,"169 cm",174,"177 cm",165,"158 cm",181,"173 cm",162];
+
+console.log("for biasa")
+for (let i=0; i < tinggiBadan.length; i++) {
+   console.log(tinggiBadan[i]);
+}
+console.log("");
+console.log("for of")
+for (let i of tinggiBadan) {
+   console.log(i);
+}
